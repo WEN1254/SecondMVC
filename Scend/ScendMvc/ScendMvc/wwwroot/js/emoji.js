78c7a52ceb300565af9096285ad07dcfc79ea46c
子鵬 李 <hawk419383@gmail.com>
2020-08-22 11:58:06 +0800

    1111

---
+++ Scend/ScendMvc/ScendMvc/wwwroot/js/emoji.js
@@ -0,0 +1,34 @@
+﻿
+var category = ['', '😀', '😁', '😡', '🦅', '🐓', '🍌', '🚗', '🍔'];
+
+//$.ajax({
+//    url:'https://emoji-api.com/categories?access_key=6219f172e6b9b78d96fb8d926869fd53fae941ae',
+//    type: 'GET',
+//    data: '',
+//    async: true,
+//    success: function (response) {
+
+//        category = response;
+//        debugger;
+//        CreateCategory();
+//    }
+//})
+
+//function CreateCategory() {
+//}
+var emoji = document.getElementById('emoji');
+
+category.forEach(x => {
+    var emojibtn = document.createElement('option')
+    emojibtn.innerHTML = x;
+
+    emoji.appendChild(emojibtn);
+
+
+});
+function push() {
+    const x = document.getElementById('emoji');
+    const num = x.selectedIndex;
+    document.getElementById('msg').value += category[num];
+
+}
