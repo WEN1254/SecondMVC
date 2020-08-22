d6cdff2d174f5755d50b46461b904bff737c5083
子鵬 李 <hawk419383@gmail.com>
2020-08-22 20:50:10 +0800

    444444

---
+++ Scend/ScendMvc/ScendMvc/wwwroot/js/getitem.js
@@ -0,0 +1,22 @@
+﻿var member;
+
+
+$.ajax({
+    url: '/api/AspNetUsers',
+    type: 'GET',
+    data: '',
+    async: true,
+    success: function (response) {
+        var email = localStorage.getItem('Email');
+        console.log(email);
+        member = response.find(x => x.userName == email);
+        
+        test._data.present = member.Present;
+
+
+        
+
+    }
+})
+
+
