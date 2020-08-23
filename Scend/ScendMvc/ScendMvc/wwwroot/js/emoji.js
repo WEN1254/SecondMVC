eafa013427d1b0186b602cc45d0f7b5a6eb71b33
子鵬 李 <hawk419383@gmail.com>
2020-08-24 00:10:49 +0800

    22

--- Scend/ScendMvc/ScendMvc/wwwroot/js/emoji.js
+++ Scend/ScendMvc/ScendMvc/wwwroot/js/emoji.js
@@ -2,41 +2,44 @@
 //var category = ['', '😀', '😁', '😡', '🦅', '🐓', '🍌', '🚗', '🍔'];
 var category;
 $.ajax({
-    url:'https://emoji-api.com/emojis?access_key=6219f172e6b9b78d96fb8d926869fd53fae941ae',
+    url: 'https://emoji-api.com/emojis?access_key=6219f172e6b9b78d96fb8d926869fd53fae941ae',
     type: 'GET',
     data: '',
     async: true,
     success: function (response) {
 
         category = response;
-        
+
         CreateCategory(category);
     }
 })
 
 function CreateCategory(x) {
-    const groupBy = (array, key) => {
-        // Return the end result
-        return array.reduce((result, currentValue) => {
-            // If an array already present for key, push it to the array. Else create an array and push the object
-            (result[currentValue[key]] = result[currentValue[key]] || []).push(
-                currentValue
-            );
-            // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
-            return result;
-        }, {}); // empty object is the initial value for result object
+    Array.prototype.groupBy = function (prop) {
+        return this.reduce(function (groups, item) {
+            const val = item[prop]
+            groups[val] = groups[val] || []
+            groups[val].push(item)
+            return groups
+        }, {})
     };
-    const groupbycharacter = groupBy(x, 'group');
-    groupbycharacter.forEach(x => {
-    var emojibtn = document.createElement('option')
-    emojibtn.innerHTML = x;
+    const groupbycharacter = x.groupBy('group');
+    console.log(`${groupbycharacter}`)
+    ary = new Array();
+    for (x in groupbycharacter) ary[ary.length] = x;
+    debugger;
 
-    emoji.appendChild(emojibtn);
+    //groupbycharacter.forEach(x => {
+    //var emojibtn = document.createElement('option')
+    //emojibtn.innerHTML = x;
+
+    //emoji.appendChild(emojibtn);
 
 
-});
+    //});
     debugger;
 }
+
 
 
 
