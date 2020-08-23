ff0e559dd82b58fd02fa033d6b90ea7cfcb6ede9
子鵬 李 <hawk419383@gmail.com>
2020-08-23 23:16:51 +0800

    ffffff

--- Scend/ScendMvc/ScendMvc/wwwroot/js/emoji.js
+++ Scend/ScendMvc/ScendMvc/wwwroot/js/emoji.js
@@ -1,24 +1,33 @@
 ﻿
-var category = ['', '😀', '😁', '😡', '🦅', '🐓', '🍌', '🚗', '🍔'];
+//var category = ['', '😀', '😁', '😡', '🦅', '🐓', '🍌', '🚗', '🍔'];
+var category;
+$.ajax({
+    url:'https://emoji-api.com/emojis?access_key=6219f172e6b9b78d96fb8d926869fd53fae941ae',
+    type: 'GET',
+    data: '',
+    async: true,
+    success: function (response) {
 
-//$.ajax({
-//    url:'https://emoji-api.com/categories?access_key=6219f172e6b9b78d96fb8d926869fd53fae941ae',
-//    type: 'GET',
-//    data: '',
-//    async: true,
-//    success: function (response) {
+        category = response;
+        
+        CreateCategory(category);
+    }
+})
 
-//        category = response;
-//        debugger;
-//        CreateCategory();
-//    }
-//})
-
-//function CreateCategory() {
-//}
-var emoji = document.getElementById('emoji');
-
-category.forEach(x => {
+function CreateCategory(x) {
+    const groupBy = (array, key) => {
+        // Return the end result
+        return array.reduce((result, currentValue) => {
+            // If an array already present for key, push it to the array. Else create an array and push the object
+            (result[currentValue[key]] = result[currentValue[key]] || []).push(
+                currentValue
+            );
+            // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+            return result;
+        }, {}); // empty object is the initial value for result object
+    };
+    const groupbycharacter = groupBy(x, 'group');
+    groupbycharacter.forEach(x => {
     var emojibtn = document.createElement('option')
     emojibtn.innerHTML = x;
 
@@ -26,9 +35,34 @@ category.forEach(x => {
 
 
 });
-function push() {
-    const x = document.getElementById('emoji');
-    const num = x.selectedIndex;
-    document.getElementById('msg').value += category[num];
-
+    debugger;
 }
+
+
+
+//var emoji = document.getElementById('emoji');
+
+//category.forEach(x => {
+//    var emojibtn = document.createElement('option')
+//    emojibtn.innerHTML = x;
+
+//    emoji.appendChild(emojibtn);
+
+
+//});
+//function push() {
+//    const x = document.getElementById('emoji');
+//    const num = x.selectedIndex;
+//    document.getElementById('msg').value += category[num];
+
+//}
+
+var face={
+    "slug": "beaming-face-with-smiling-eyes",
+        "character": "\ud83d\ude01",
+            "unicodeName": "beaming face with smiling eyes",
+                "codePoint": "1F601",
+                    "group": "smileys-emotion",
+                        "subGroup": "face-smiling"
+};
+console.log(`${face.slug},${face.character},${face.codePoint}`);
